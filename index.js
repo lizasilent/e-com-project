69914f5fb6f6f300c0cec1291eb1e14c695c22b0
Liza_silent <elizavetad@ya.ru>
2022-09-06 14:34:48 +0300

    render fix 2

--- index.js
+++ index.js
@@ -26,27 +26,27 @@ const card = document.querySelector(".catalog__card");
 
 
 
-
-
-
 function createCard(product) {
 
-const gridElement = catalog.cloneNode(true);
-const cardDescription = gridElement.querySelector(".card__description");
-const cardPrice = gridElement.querySelector(".card__price");
+const newcard = card.cloneNode(true);
+
+const cardDescription = card.querySelector(".card__description");
+const cardPrice = card.querySelector(".card__price");
 
 cardDescription.textContent = product.name;
 cardPrice.textContent = product.price;
    
-return gridElement;
+return newcard;
 }
+
+console.log()
 
 
 function renderCard(data) {
-  console.log(data)
 catalog.prepend(createCard(data))
 }
 
 items.forEach(function(data) {
   renderCard(data);
+  console.log(renderCard(data))
 });
